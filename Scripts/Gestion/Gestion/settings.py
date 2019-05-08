e6c3adfbbae30b26cee654f42ce4dd1f8ac601be
arroschino60 <sonykuun60@gmail.com>
2019-05-08 05:47:06 -0500

    test1

--- Scripts/Gestion/Gestion/settings.py
+++ Scripts/Gestion/Gestion/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Gestion.urls'
@@ -124,4 +126,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),)
