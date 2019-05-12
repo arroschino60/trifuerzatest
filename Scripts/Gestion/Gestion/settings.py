a8d7f5c828c66642ae7324ce9e13823b00d740e9
arroschino60 <sonykuun60@gmail.com>
2019-05-11 21:03:50 -0500

    k

--- Scripts/Gestion/Gestion/settings.py
+++ Scripts/Gestion/Gestion/settings.py
@@ -127,3 +127,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = '/home/Sonykuun/trifuerza/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
